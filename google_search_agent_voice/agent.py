b918e1d34f4d02ff7ff6521209039d021c88e184
zyper16 <zyper16@gmail.com>
2025-08-20 18:19:39 +0300

    Added a second agent

---
+++ google_search_agent_voice/agent.py
@@ -0,0 +1,15 @@
+from google.adk.agents import Agent
+from google.adk.tools import google_search  # Import the tool
+
+root_agent = Agent(
+   # A unique name for the agent.
+   name="basic_search_agent",
+   # LLMs list: https://google.github.io/adk-docs/get-started/streaming/quickstart-streaming/#supported-models
+   model="gemini-live-2.5-flash-preview",  # for example: model="gemini-2.0-flash-live-001" or model="gemini-2.0-flash-live-preview-04-09"
+   # A short description of the agent's purpose.
+   description="Agent to answer questions using Google Search.",
+   # Instructions to set the agent's behavior.
+   instruction="You are an expert researcher. You always stick to the facts.",
+   # Add google_search tool to perform grounding with Google search.
+   tools=[google_search]
+)
--- multi_tool_agent/agent.py
+++ multi_tool_agent/agent.py
@@ -57,6 +57,7 @@ def get_current_time(city: str) -> dict:
 root_agent = Agent(
     name="weather_time_agent",
     model="gemini-2.0-flash",
+    # model="gemini-live-2.5-flash-preview",
     description=(
         "Agent to answer questions about the time and weather in a city."
     ),
